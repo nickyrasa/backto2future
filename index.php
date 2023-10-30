6fdcbd130b50fc4ea96c5862ae21e28f09c5d947
nickyrasa <144258315+nickyrasa@users.noreply.github.com>
2023-10-30 22:21:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$presentTime = new DateTime('2015-10-21 04:29:00');
-$destinationTime = new DateTime('2023-10-31 12:00:00');
+$presentTime = new DateTime('2013-08-15 14:00:00');
+$destinationTime = new DateTime('2023-10-31 22:21:00');
 
 $formattedPresentTime = $presentTime->format('M-d-Y A g:i');
 $formattedDestinationTime = $destinationTime->format('M-d-Y A g:i');
