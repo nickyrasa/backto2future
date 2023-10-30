301d50d6de0bed561eeeeecb8daab51e734bbb6d
nickyrasa <144258315+nickyrasa@users.noreply.github.com>
2023-10-30 22:24:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,3 +6,6 @@ $destinationTime = new DateTime('2023-10-31 22:21:00');
 $formattedPresentTime = $presentTime->format('M-d-Y A g:i');
 $formattedDestinationTime = $destinationTime->format('M-d-Y A g:i');
 $interval = $presentTime->diff($destinationTime);
+
+ echo "Date de départ :" . $formattedPresentTime ;
+ echo "Date de destination : " . $formattedDestinationTime;
